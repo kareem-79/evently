e71e1d9b39c39f9610c8ecb5680ff5945dc1524a
kareem-79 <kareemmohamed2467@gmail.com>
2025-09-08 21:51:19 +0300

    authentication finish

--- lib/features/authentication/login/login_screen.dart
+++ lib/features/authentication/login/login_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import '../../../core/resources/assets_manager.dart';
+import '../../../core/utils/validation.dart';
 import '../../../core/widget/custom_text_form_filed.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -18,6 +19,24 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   late bool secure = true;
+  var formKey=GlobalKey<FormState>();
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    emailController=TextEditingController();
+    passwordController=TextEditingController();
+  }
+  @override
+  void dispose() {
+    // TODO: implement dispose
+
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,130 +44,153 @@ class _LoginScreenState extends State<LoginScreen> {
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.0.sp),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SizedBox(height: 40.h),
-              Image.asset(ImageAssets.eventlyLogo, height: 186.h, width: 136.w),
-              SizedBox(height: 24.h),
-              CustomTextFormFiled(
-                keyboardType: TextInputType.emailAddress,
-                label: "E-mail",
-                prefixIcon: Icons.email,
-              ),
-              CustomTextFormFiled(
-                secure: secure,
-                label: "Password",
-                prefixIcon: Icons.lock,
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    secure = !secure;
-                    setState(() {});
+          child: Form(
+            key: formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 40.h),
+                Image.asset(ImageAssets.eventlyLogo, height: 186.h, width: 136.w),
+                SizedBox(height: 24.h),
+                CustomTextFormFiled(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  label: "E-mail",
+                  prefixIcon: Icons.email,
+                  validator: (input) {
+                    if(input==null||input.trim().isEmpty){
+                      return "E-mail is Required";
+                    }
+                    if(!Validation.isValidateEmail(input)){
+                      return "E-mail is not validate";
+                    }
+                    return null;
                   },
-                  icon: Icon(secure?Icons.visibility_off:Icons.visibility),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.0.sp),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    CustomTextButton(onPress: () {}, text: "Forget Password ?"),
-                  ],
+                CustomTextFormFiled(
+                  controller: passwordController,
+                  secure: secure,
+                  label: "Password",
+                  prefixIcon: Icons.lock,
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      secure = !secure;
+                      setState(() {});
+                    },
+                    icon: Icon(secure?Icons.visibility_off:Icons.visibility),
+                  ),
+                  validator: (input) {
+                    if(input==null||input.trim().isEmpty){
+                      return "Password is Required";
+                    }
+                    if(input.length<8){
+                      return "Sorry,Password should be at least 8 char";
+                    }
+                    return null;
+                  },
                 ),
-              ),
-              CustomElevatedButton(
-                onPress: () {
-                  Navigator.pushReplacementNamed(
-                    context,
-                    RoutesManager.register,
-                  );
-                },
-                text: "Login",
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 8.sp),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Don’t Have Account ?",
-                      style: Theme.of(context).textTheme.titleSmall,
-                    ),
-                    CustomTextButton(
-                      onPress: () {
-                        Navigator.pushReplacementNamed(
-                          context,
-                          RoutesManager.register,
-                        );
-                      },
-                      text: "Create Account",
-                    ),
-                  ],
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0.sp),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      CustomTextButton(onPress: () {}, text: "Forget Password ?"),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 16.h),
-              Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                      thickness: 1.sp,
-                      color: ColorsManager.blue,
-                      indent: 26,
-                      endIndent: 16,
-                    ),
-                  ),
-                  Text(
-                    "Or",
-                    style: GoogleFonts.inter(
-                      fontSize: 16.sp,
-                      color: ColorsManager.blue,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      thickness: 1.sp,
-                      color: ColorsManager.blue,
-                      indent: 16,
-                      endIndent: 26,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 24.h),
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12.r),
-                  ),
-                  side: BorderSide(color: ColorsManager.blue),
+                CustomElevatedButton(
+                  onPress: _login,
+                  text: "Login",
                 ),
-                onPressed: () {},
-                child: Padding(
-                  padding: EdgeInsets.all(12.0.sp),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.sp),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Image.asset(ImageAssets.googleLogo),
-                      SizedBox(width: 10.w),
                       Text(
-                        "Login With Google",
-                        style: GoogleFonts.inter(
-                          fontSize: 20.sp,
-                          fontWeight: FontWeight.bold,
-                          color: ColorsManager.blue,
-                        ),
+                        "Don’t Have Account ?",
+                        style: Theme.of(context).textTheme.titleSmall,
+                      ),
+                      CustomTextButton(
+                        onPress: () {
+                          Navigator.pushReplacementNamed(
+                            context,
+                            RoutesManager.register,
+                          );
+                        },
+                        text: "Create Account",
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 16.h),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 1.sp,
+                        color: ColorsManager.blue,
+                        indent: 26,
+                        endIndent: 16,
+                      ),
+                    ),
+                    Text(
+                      "Or",
+                      style: GoogleFonts.inter(
+                        fontSize: 16.sp,
+                        color: ColorsManager.blue,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 1.sp,
+                        color: ColorsManager.blue,
+                        indent: 16,
+                        endIndent: 26,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 24.h),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.r),
+                    ),
+                    side: BorderSide(color: ColorsManager.blue),
+                  ),
+                  onPressed: () {},
+                  child: Padding(
+                    padding: EdgeInsets.all(12.0.sp),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(ImageAssets.googleLogo),
+                        SizedBox(width: 10.w),
+                        Text(
+                          "Login With Google",
+                          style: GoogleFonts.inter(
+                            fontSize: 20.sp,
+                            fontWeight: FontWeight.bold,
+                            color: ColorsManager.blue,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
-    ;
+
+  }
+  void _login() {
+    if (formKey.currentState?.validate() ?? false) {
+      print("Now login...");
+    }
   }
 }
