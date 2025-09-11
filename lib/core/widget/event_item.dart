ead513fb269ab5bd5552fa9969aa47e383338a04
kareem-79 <kareemmohamed2467@gmail.com>
2025-09-11 19:17:13 +0300

    finish homeTab

--- lib/core/widget/event_item.dart
+++ lib/core/widget/event_item.dart
@@ -2,7 +2,6 @@ import 'package:evently/core/resources/assets_manager.dart';
 import 'package:evently/core/resources/colors_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class EventItem extends StatelessWidget {
   const EventItem({super.key});
